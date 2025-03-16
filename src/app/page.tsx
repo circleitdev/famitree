287c4591191a5ecfdd79bb47616a63eb89b601bb
rasvanjaya21 <mrsejati17@gmail.com>
2025-03-16 18:55:34 +0700

    feat(app): release dummy data(s)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 'use client';
 
 import dynamic from 'next/dynamic';
+import { dummyDatas } from '@/utils/constans';
 
 const Renderer = dynamic(() => import('@/components/renderer/renderer'), { ssr: false });
 
 export default function Home() {
-	return <Renderer id={''} title={''} nodes={[]} updatedAt={new Date()} />;
+	return <Renderer id={''} title={''} nodes={dummyDatas} updatedAt={new Date()} />;
 }
