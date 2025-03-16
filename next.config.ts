506d21cfee1166dbd9b0e77530d88a2c53bc885d
rasvanjaya21 <mrsejati17@gmail.com>
2025-03-16 18:57:06 +0700

    chore(core): add image remote pattern template

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
 	/* config options here */
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'TOBEMODIFIED',
+				hostname: 'TOBEMODIFIED',
+				pathname: 'TOBEMODIFIED',
+			},
+		],
+		dangerouslyAllowSVG: true,
+	},
 };
 
 export default nextConfig;
